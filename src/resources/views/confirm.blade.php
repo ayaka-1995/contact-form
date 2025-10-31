61499c8711fb06f1b47d90130c16e9bb9e359e79
isii ayaka <ayaka950210@gmail.com>
2025-10-31 23:12:55 +0900

    修正

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -1,22 +1,11 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Form</title>
-    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/confirm.css') }}">
-</head>
-<body>
-    <header class="header">
-        <div class="header__inner">
-            <a class="header__logo" href="">Contact Form</a>
-        </div>
-    </header>
+@extends('layouts.app')
 
-    <main>
-        <div class="confirm__content">
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/confirm.css') }}">
+@endsecion
+
+@section('content')
+<div class="confirm__content">
             <div class="confirm__heading">
                 <h2>お問い合わせ内容</h2>
             </div>
@@ -55,6 +44,4 @@
                 </div>
             </form>
         </div>
-    </main>
-</body>
-</html>
+@endsection
